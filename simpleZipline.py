93f639c93ff230d9977ca2f45caaac6c7a927a12
Sam Ingersoll <sam.ingersoll@gmail.com>
2017-02-19 16:43:26 -0800

    initial tensorboard support

--- simpleZipline.py
+++ simpleZipline.py
@@ -1,19 +1,18 @@
-
 import tensorflow as tf
 from zipline.api import order, record, symbol, sid
-import tensorboard
-
+from tensorboard import Tensorboard
 
 def initialize(context):
-    pass
-
+    context.tensorboard = Tensorboard()
 
 def handle_data(context, data):
-   # order(symbol('AAPL'), 10)
+    #order(symbol('AAPL'), 10)
     #record(AAPL=data.current(symbol('AAPL'), 'price'))
-    #print( data.current(symbol('AAPL'), 'price'))
-    tf.summary.scalar('price', data.current(symbol('AAPL'), 'price'))
+    print( data.current(symbol('AAPL'), 'price'))
+    print( context.get_datetime() )
+    context.tensorboard.log( 'price', \
+                              data.current(symbol('AAPL'), 'price'), \
+                              context.get_datetime().date() )
+    # tf.summary.scalar('price', data.current(symbol('AAPL'), 'price'))
 
 
-if __name__ == '__main__':
-    pass
--- tensorboard.py
+++ tensorboard.py
@@ -1,10 +1,20 @@
 import tensorflow as tf
+import datetime
+import os
 
 class Tensorboard:
-    def __init__(self, log_dir='./logs', max_queue=10, flush_secs=120):
-        self.log_dir = log_dir
-        self.merged = tf.merge_all_summaries()
-        self.writer = tf.train.SummaryWriter(self.log_dir,
+    def __init__( self, max_queue=10, flush_secs=12):
+        self.log_dir = os.getcwd()+"/logs"
+        self.merged = tf.summary.merge_all()
+        self.writer = tf.summary.FileWriter(self.log_dir,
                                              max_queue=max_queue,
                                              flush_secs=flush_secs,
                                              graph_def=None)
+    def log( self, name, value, datetime ):
+        epoch = datetime.toordinal()
+        summary = tf.Summary()
+        summary_value = summary.value.add()
+        summary_value.simple_value = value
+        summary_value.tag = name
+        self.writer.add_summary( summary, global_step=epoch )
+        self.writer.flush()
