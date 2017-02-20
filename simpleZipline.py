0b7f2a4b657e76c7ad056dc7fda29e55c31d5207
Ro-ee Tal <roeetal1@gmail.com>
2017-02-19 16:10:37 -0800

    zipline data

--- simpleZipline.py
+++ simpleZipline.py
@@ -5,15 +5,21 @@ import tensorboard
 
 
 def initialize(context):
-    pass
-
+    context.sym = symbol('AAPL')
+    context.order_target = 10
+    context.fields = ["price","open","close","high","low"]
 
 def handle_data(context, data):
-   # order(symbol('AAPL'), 10)
-    #record(AAPL=data.current(symbol('AAPL'), 'price'))
-    #print( data.current(symbol('AAPL'), 'price'))
-    tf.summary.scalar('price', data.current(symbol('AAPL'), 'price'))
+    price_history = data.history(context.sym, context.fields, bar_count=2, frequency="1d")
+    print(price_history)
+    for s in context.fields:
+        prev_bar = price_history[s][-2]
+        curr_bar = price_history[s][-1]
+        #if curr_bar > prev_bar:
+            #order(s, context.order_target)    
+
+    #tf.summary.scalar('today_price',tday_price) 
 
 
-if __name__ == '__main__':
-    pass
+#if __name__ == '__main__':
+#    pass
