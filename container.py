31590e79fd3f53ae0404fe178f490c6b2f503d6f
Ro-ee Tal <roeetal1@gmail.com>
2017-02-21 17:27:48 -0800

    textfile writes

--- container.py
+++ container.py
@@ -32,32 +32,36 @@ def initialize( algo ):
     #else:
     #    algo.filewriter = None    
     algo.fields = ["price","open","close","high","low"]
-    algo.columns = len(algo.fields)
-    #algo.network.train()
+    algo.network.train()
+    heading = []
     for i in range(len(algo.stocks)):
-        for j in range(len(algo.fields)):    
-            algo.text_file_data.append(str(algo.stocks[i].symbol)+"_"+algo.fields[j])
+            heading.append(str(algo.stocks[i].symbol)+"_"+algo.fields[algo.write_fields[i]])
+    algo.text_file_data.append(heading)
 
 def handle_data( algo, data):
     algo.day += 1
     price_history = []
     if ( algo.day >= 3 ):
-
-        #print( "-------------Handle Data-------------" )
+        rows = []
         for i in range(len(algo.stocks)):
-            price_history.append( data.history(algo.stocks[i], algo.fields,\
-                                     bar_count=2, frequency="1d").values.tolist())
-            algo.filewriters[i].log( 'price',\
-                                      price_history[i][1][0],\
-                                      algo.get_datetime().date() )
-            algo.filewriters[i].log( 'change',\
-                                      price_history[i][1][3]-\
-                                      price_history[i][1][0],\
-                                      algo.get_datetime().date() )
+            price_history.append (data.history(algo.stocks[i], algo.fields[algo.write_fields[i]],\
+                                     bar_count=1, frequency="1d").values.tolist())
+            #algo.filewriters[i].log( 'price',\
+             #                         price_history[i][1][0],\
+              #                        algo.get_datetime().date() )
+            #algo.filewriters[i].log( 'change',\
+             #                         price_history[i][1][3]-\
+              #                        price_history[i][1][0],\
+               #                       algo.get_datetime().date() )
+            rows.append(price_history[i][algo.write_fields[i]])
             algo.filewriters[i].writer.flush()
-        algo.text_file_data.append("\n"+price_history)
-        #prev_bar = list(price_history[i][0] for i in range(len(price_history)))
-        #curr_bar = list(price_history[i][0] for i in range(len(price_history)))
+        algo.text_file_data.append(rows)
+    if (algo.day == (algo.liveday-algo.start).days):
+        with open(algo.input_data_dir+"/data.csv", 'w') as csvfile:
+            algo.writer = csv.writer(csvfile)
+            algo.writer.writerows(algo.text_file_data)
+
+    
 
 def analyze( algo, results ):
     algo.filewriter.writer.close()
@@ -66,13 +70,13 @@ def analyze( algo, results ):
         writer.writerows(algo.text_file_data)
 
 class Container:
-    def __init__( self, learning_rate, max_steps, hidden1, hidden2, batch_size, lookback=2, input_data_dir=None, log_dir=None, stocks=['AAPL','CAT','NVDA'], fields=[0,0,0], start=datetime(2000,1,1,0,0,0,0,pytz.utc), end=datetime(2016,1,1,0,0,0,0,pytz.utc), liveday=datetime(2011,1,1,0,0,0,0,pytz.utc), individual_name=get_monster(), generation_number=0 ):
+    def __init__( self, learning_rate, max_steps, hidden1, hidden2, batch_size, lookback=2, input_data_dir=None, log_dir=None, stocks=['AAPL','CAT','NVDA'], fields=[0,0,0], start=datetime(2010,1,1,0,0,0,0,pytz.utc), end=datetime(2016,1,1,0,0,0,0,pytz.utc), liveday=datetime(2011,1,1,0,0,0,0,pytz.utc), individual_name=get_monster(), generation_number=0 ):
         self.log_dir = log_dir
         self.input_data_dir = input_data_dir
         self.individual_name = individual_name
         self.generation_number = generation_number
         self.stocks = stocks     # ['AAPL','CAT','NVDA']
-        self.fields = fields
+        self.write_fields = fields
         self.start = start  # datetime(2011, 1, 1, 0, 0, 0, 0, pytz.utc)
         self.end = end      # datetime(2016, 1, 1, 0, 0, 0, 0, pytz.utc)
         self.classes = 2
@@ -81,21 +85,22 @@ class Container:
         if self.input_data_dir is None:
             self.input_data_dir = self.log_dir
         # Load price data from yahoo.
-        data = load_bars_from_yahoo(stocks=self.stocks, indexes={},\
+        self.data = load_bars_from_yahoo(stocks=self.stocks, indexes={},\
                                  start=start, end=end, adjusted = True)
-        data = data.dropna()
+        self.data = data.dropna()
         # Create and run the algorithm.
-        algorithm = TradingAlgorithm(handle_data=handle_data,\
+        self.algorithm = TradingAlgorithm(handle_data=handle_data,\
                                  initialize=initialize) #,\
                                  #identifiers=STOCKS)
-        algorithm.stocks = self.stocks
-        algorithm.fields = self.fields
-        algorithm.start = start
-        algorithm.end = end
-        algorithm.liveday = liveday
-        algorithm.input_data_dir = self.input_data_dir
-        algorithm.log_dir = self.log_dir # os.getcwd()+'/logs/'+time.strftime('%d_%m_%Y-%H_%M_%S',time.gmtime())+' = %.2f' % eps
+        self.algorithm.stocks = self.stocks
+        self.algorithm.write_fields = self.write_fields
+        self.algorithm.start = start
+        self.algorithm.end = end
+        self.algorithm.liveday = liveday
+        self.algorithm.input_data_dir = self.input_data_dir
+        self.algorithm.log_dir = self.log_dir # os.getcwd()+'/logs/'+time.strftime('%d_%m_%Y-%H_%M_%S',time.gmtime())+' = %.2f' % eps
         #algorithm.network = Network( learning_rate, max_steps, hidden1, hidden2, batch_size, self.input_data_dir, self.log_dir, (liveday-start).days, len(self.stocks), self.classes)
-        algorithm.network = Network( max_steps = 3 )
+        self.algorithm.network = Network( max_steps = 3 )
         print( self.log_dir )
-        results = algorithm.run(data)
+    def run():
+        results = self.algorithm.run(self.data)
